ac9f6238c8bc26f0e4f60503489d5a09c04c3b9f
Ram Chinniah <rchinniah@thedmsgrp.com>
2018-04-15 22:14:10 -0400

    Deleted files :wq

--- features/support/env.rb
+++
@@ -1,8 +0,0 @@
-require "capybara/cucumber"
-require "selenium-webdriver"
-
-Capybara.configure do |config|
-  config.run_server = false
-  config.default_driver = :selenium_chrome
-  config.app_host = "http://takehome.zeachable.com"
-end
